036c94f73c2013333251f7769e3fbafc325519b0
AineKiraboMbabazi <mbabaziainekirabo@gmail.com>
2019-05-18 10:23:20 +0200

    add disease diagnosis api

---
+++ diagnosis_api/controllers/get_disease_details.py
@@ -0,0 +1,108 @@
+import pandas as pd 
+import os
+
+
+class Disease_details:
+    """
+    Disease details model that defines the disease_details
+    """
+
+    def __init__(self,file_name,disease_name):
+        self.file_name = file_name
+        # convert disease name to lower case for uniformity
+        self.disease_name = disease_name.lower()
+        self.causes = []
+        self.symptoms = []
+        self.Treatments = []
+
+    def load_disease_file(self):
+        """ 
+        load_disease_file function loads the disease excel file
+        : return diseases dataframe:
+        """
+
+        load_disease_files = pd.read_excel('../dataset/{}'.format(self.file_name))
+        
+        
+        #  convert to dataframe
+        df = pd.DataFrame(load_disease_files)
+
+        # autofill empty spaces with 0
+        data = df.fillna(value = 0)
+        return data
+
+
+    def get_disease_detail_param_details(self,query_string):
+        """ 
+        gets the details of causes,symptoms,treatment for a particular disease
+        : param causes or symptoms or treatment :
+        
+        """
+
+       
+        # check for non excel files
+        
+        data = self.load_disease_file()
+        
+        # group data by disease
+        disease_grouped_data = data.groupby('Disease').apply(lambda g: pd.Series(g[query_string].values)).rename(columns=lambda x: 'query_string%s' % x)
+        
+        for  row in disease_grouped_data[self.disease_name]: 
+            if ((row != 0) &( query_string == 'Causes')):
+                self.causes.append(row)
+
+            if ((row != 0) & (query_string == 'Symptoms')):
+                self.symptoms.append(row)
+                
+            if ((row != 0) & (query_string == 'Treatment')):
+                self.Treatments.append(row) 
+
+
+
+    def get_disease_details(self): 
+        """ 
+        Get disease details
+        : Return disease details :
+
+        """
+        # load disease file
+        file_name = self.file_name
+        basepath = '../dataset/'
+        files = []
+        
+        for entry in os.listdir(basepath):
+            if os.path.isfile(os.path.join(basepath, entry)):
+                
+                files.append(entry)
+        # print(file_name)
+        if file_name.lower() not in files:
+            
+            return('File with that name doesnot exist')
+                
+        if (not file_name.lower().endswith(('.xls','.xlsx'))):
+            
+            return(file_name.lower(),'Dataset file format not supported, only excel files are accepted')
+
+        # disease Causes
+        self.get_disease_detail_param_details('Causes') 
+        
+        # disease symptoms
+        self.get_disease_detail_param_details('Symptoms') 
+        
+        # disease Treatment
+        self.get_disease_detail_param_details('Treatment')
+        
+        disease_details = {
+            'Disease_Name':self.disease_name,
+            'Causes':self.causes,
+            'Symptoms':self.symptoms,
+            'Treatment':self.Treatments
+        }
+        return( disease_details)
+
+    
+disease = Disease_details('rabbit.txt','snuffles')
+print(disease.get_disease_details())
+
+disease = Disease_details('rabbit_diseases.xls','tyzzers_disease')
+print(disease.get_disease_details())
